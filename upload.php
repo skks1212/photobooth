d64642a2d56414e7e17f1bef677a7d403ea2cd62
Skks1212 <kacker.shivank@gmail.com>
2023-04-28 10:30:35 +0530

    add

--- upload.php
+++ upload.php
@@ -1,4 +1,11 @@
 <?php
+
+// show all errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $price = $_POST['price'];
